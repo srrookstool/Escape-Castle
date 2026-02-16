856205798c46de9bcf9993f58e7d7945fc9abbe4
hannahd049 <HGDees01@indianatech.net>
2026-02-16 11:44:53 -0500

    update MAIN.py

--- OOProject/MAIN.py
+++ OOProject/MAIN.py
@@ -192,15 +192,18 @@ musicnote_A=Object("Music Note A", "You see a large music note barely hanging on
 musicenote_CE=Object("Music Note C and E", "You see a large music note barely hanging on the wall, it is the note CE, and it is the only one that is not covered in dust. You examine it, and you notice that there is a small inscription on the back of the note that says 'The key to the ballroom is in the music'.")
 FCnote2=Object("A small ripped note", "You see a small ripped note on the ground, badly worn, you pick it up and read the numbers on it- it contains two digits - piece of paper for the final code: 16.")
 piano=Puzzle("Grand Piano", "You see a grand piano in the corner of the ballroom, it is covered in dust, but it looks like it is still functional. You sit down and start to play the notes you found in the foyer and library, and as you play, you notice that the music starts to change- the top of the paino opens when you play the correct notes, revealing an opening. Enter the notes...")
-#Dungeon objects
 
-finaldoor=Puzzle("Final Door", "You uncover a set of cellar doors-hoping they head outside you think about what you have found so far- You try to piece together the clues and figure out the code to open the door.")
+#Dungeon objects
+bench=Object("Bench", "You see a bench made of rock in the corner of the dungeon, you walk over to examine it and you see a multiple carvings of combinations of the same four numbers from the notes all over the bench... what could this mean?")
+coveredTable=Clue("Covered Table", "You see a covered table in the corner of the dungeon, you pull off the cover while coughing from the dust you drag the table tunder  the cellar doors hoping to reach the exit...")#coveredTable must be moved under the cellar doors to reveal the final puzzle
+finaldoor=Puzzle("Final Door", "You uncover a set of cellar doors-hoping they head outside you think about what you have found so far- You try to piece together the clues and figure out the code to open the door. Enter your choices carefully as there may be a consequence... :")
 
 #Randomized objects
 VintageThrone=Object("Vintage Throne", "You see a large, ornate throne in the center of the dungeon. It is made of dark wood and has intricate carvings. Do you dare to sit down..?")#50/50- sleep potion-lose all time down to 5 minutes, or energizer potion- full restoration of time 
 handMirror=Object("Hand Mirror", "You see a small hand mirror on the ground, it is old and cracked, but it still reflects your image. As you look into the mirror, you see a faint image of a ghostly figure behind you. Do you dare to look again..?")#if yes get 5 minute resoration, if no, nothing happens
 
 # --- ADD OBJECTS TO ROOMS ---
+foyer.objects.append(FCnote1)
 foyer.objects.append(large_chest)
 foyer.objects.append(musicnote_G)
 foyer.objects.append(clockCH)
@@ -211,6 +214,10 @@ library.objects.append(desk)
 library.objects.append(musicnote_A)
 ballroom.objects.append(musicenote_CE)
 ballroom.objects.append(piano)
+ballroom.objects.append(FCnote2)
+dungeon.objects.append(bench)
+dungeon.objects.append(coveredTable)
+dungeon.objects.append(finaldoor)      
 
 
 # main game loop
